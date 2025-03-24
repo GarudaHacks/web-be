b3c0ae5ae99115721ba15db526546d06c35fb8b8
Hibatullah Fawwaz Hana <hiba.kudus@gmail.com>
2025-03-24 23:01:51 +0700

    feat: login

--- functions/src/controllers/auth_controller.ts
+++ functions/src/controllers/auth_controller.ts
@@ -5,8 +5,55 @@ import axios from "axios";
 /**
  * Logs in user
  */
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-export const login = async (req: Request, res: Response): Promise<void> => {};
+export const login = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
+
+  try {
+    const isEmulator = process.env.FUNCTIONS_EMULATOR === "true";
+
+    const user = await db.collection("users").where("email", "==", email).get();
+    if (user.empty) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+
+    const userDoc = user.docs[0];
+    const userId = userDoc.id;
+    const userData = userDoc.data();
+
+    let token;
+    if (isEmulator) {
+      const customToken = await auth.createCustomToken(userId);
+      token = {
+        idToken: customToken,
+        refreshToken: null,
+        expiresIn: 3600,
+      };
+    } else {
+      token = (
+        await axios.post(
+          `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.WEB_API_KEY}`,
+          { email, password, returnSecureToken: true }
+        )
+      ).data;
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        email: userData.email,
+        displayName: userData.name,
+      },
+      idToken: token.idToken,
+      refreshToken: token.refreshToken,
+      expiresIn: token.expiresIn,
+    });
+  } catch (error) {
+    const err = error as Error;
+    console.error("error:", err.message);
+    res.status(400).json({ error: err.message });
+  }
+};
 
 /**
  * Registers new user
@@ -22,8 +69,6 @@ export const register = async (req: Request, res: Response): Promise<void> => {
       email,
       password,
     });
-
-    console.log(`WEB_API_KEY=${process.env.WEB_API_KEY}`);
 
     let token;
     if (isEmulator) {
@@ -49,7 +94,7 @@ export const register = async (req: Request, res: Response): Promise<void> => {
     });
 
     res.status(201).json({
-      message: "User created successfully",
+      message: "Registration successful",
       user: {
         email: user.email,
         displayName: user.displayName,
