16a6a5ed28f0b96ca5f30c76c5d3b48b0e24e282
Hibatullah Fawwaz Hana <hiba.kudus@gmail.com>
2025-04-03 13:27:04 +0700

    feat: logout

--- functions/src/routes/auth.ts
+++ functions/src/routes/auth.ts
@@ -1,5 +1,11 @@
 import express, { Request, Response } from "express";
-import { login, register, refreshToken } from "../controllers/auth_controller";
+import {
+  login,
+  register,
+  refreshToken,
+  logout,
+} from "../controllers/auth_controller";
+import { validateFirebaseIdToken } from "../middlewares/auth_middleware";
 
 const router = express.Router();
 
@@ -7,6 +13,9 @@ router.post("/login", (req: Request, res: Response) => login(req, res));
 router.post("/register", (req: Request, res: Response) => register(req, res));
 router.post("/refresh-token", (req: Request, res: Response) =>
   refreshToken(req, res)
+);
+router.get("/logout", validateFirebaseIdToken, (req: Request, res: Response) =>
+  logout(req, res)
 );
 
 export default router;
