c693181249d679ed1e08e18f2c8cf27b748eaa45
Hibatullah Fawwaz Hana <hiba.kudus@gmail.com>
2025-04-18 22:01:15 +0700

    refactor: user

--- functions/src/routes/user.ts
+++ functions/src/routes/user.ts
@@ -1,9 +1,5 @@
 import express, { Request, Response } from "express";
-import {
-  createUser,
-  getUsers,
-  getCurrentUser,
-} from "../controllers/user_controller";
+import { getUsers, getCurrentUser } from "../controllers/user_controller";
 import { validateFirebaseIdToken } from "../middlewares/auth_middleware";
 
 const router = express.Router();
@@ -11,7 +7,6 @@ const router = express.Router();
 router.use(validateFirebaseIdToken);
 
 router.get("/", (req: Request, res: Response) => getUsers(req, res));
-router.post("/create", (req: Request, res: Response) => createUser(req, res));
 router.get("/me", (req: Request, res: Response) => getCurrentUser(req, res));
 
 export default router;
