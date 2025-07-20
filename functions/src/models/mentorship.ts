0f8ec49f2ab9f669bd6f9d7c50fcc421f51b08c8
Heryan Djaruma <heryandjaruma@gmail.com>
2025-07-20 14:24:14 +0700

    edit model and types

--- functions/src/models/mentorship.ts
+++ functions/src/models/mentorship.ts
@@ -9,12 +9,46 @@ export interface FirestoreMentor {
 }
 
 export interface MentorshipAppointment {
-  id?: string; // linked to doc uid in firebase, not field
+  id?: string;
   startTime: number;
   endTime: number;
   mentorId: string;
   hackerId?: string; // a hacker book for the whole team
+  teamName: string;
   hackerDescription?: string; // desc given needed by hacker
   location: string;
-  teamName?: string;
+  offlineLocation?: string; // to be filled if the location is offline
+  mentorMarkAsDone: boolean;
+  mentorMarkAsAfk: boolean; // mark if this team is AFK
+  mentorNotes: string // to give this appointment a note
+  hackerMarkAsDone: boolean;
+}
+
+export interface MentorshipAppointmentResponseAsMentor {
+	id?: string;
+  startTime: number;
+  endTime: number;
+  mentorId: string;
+  hackerId?: string;
+  hackerName: string;
+  teamName: string;
+  hackerDescription?: string; // desc given needed by hacker
+  location: string; // offline or online
+  offlineLocation?: string; // to be filled if the location is offline
+  mentorMarkAsDone: boolean;
+  mentorMarkAsAfk: boolean; // mark if this team is AFK
+  mentorNotes: string // to give this appointment a note
+}
+
+export interface MentorshipAppointmentResponseAsHacker {
+	id?: string;
+  startTime: number;
+  endTime: number;
+  mentorId: string;
+  hackerId?: string;
+  hackerName: string;
+  teamName: string;
+  hackerDescription?: string; // desc given needed by hacker
+  location: string; // offline or online
+  offlineLocation?: string; // to be filled if the location is offline
 }
--- functions/src/types/config.ts
+++ functions/src/types/config.ts
@@ -1,5 +1,3 @@
 export interface MentorshipConfig {
     isMentorshipOpen: boolean;
-    mentoringStart: number;
-    mentoringEnd: number;
 }
