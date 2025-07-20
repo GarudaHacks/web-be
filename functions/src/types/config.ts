8c56709580979808c37dbab44d2085f17701aecf
Heryan Djaruma <heryandjaruma@gmail.com>
2025-07-20 23:08:22 +0700

    edit config type

--- functions/src/types/config.ts
+++ functions/src/types/config.ts
@@ -1,3 +1,7 @@
+import { Timestamp } from "firebase-admin/firestore";
+
 export interface MentorshipConfig {
-    isMentorshipOpen: boolean;
+    isMentorshipOpen: boolean; // whether or not participant can start to book mentorship slots
+    mentorshipStartDate: Timestamp;
+    mentorshipEndDate: Timestamp;
 }
