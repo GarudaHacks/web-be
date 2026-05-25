fd31d46758d00d9c358e772cc41aa55e489932f0
Heryan Djaruma <heryandjaruma@gmail.com>
2026-05-25 10:26:44 +0800

    minor fixes types

--- functions/src/middlewares/role_middleware.ts
+++ functions/src/middlewares/role_middleware.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { auth } from "../config/firebase";
 import * as functions from "firebase-functions";
 
@@ -6,22 +6,23 @@ export const isMentor = async (
   req: Request,
   res: Response,
   next: NextFunction,
-) => {
+): Promise<void> => {
   try {
     const sessionCookie = req.cookies.__session;
     const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
     const userIsMentor = decodedClaims.mentor === true;
     if (!userIsMentor) {
-      return res.status(403).json({
+      res.status(403).json({
         status: 403,
         error: "Forbidden: Insufficient permissions",
       });
+      return;
     }
     req.user = decodedClaims;
-    return next();
+    next();
   } catch (error) {
     functions.logger.error("Error while verifying user:", error);
-    return res.status(500).json({
+    res.status(500).json({
       status: 500,
       error: "Something went wrong",
     });
