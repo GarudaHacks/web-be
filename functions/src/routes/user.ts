6f69d221901f5f453a60715bf278ef5751b3c177
Heryan Djaruma <heryandjaruma@gmail.com>
2026-06-13 17:09:24 +0800

    add delete account

--- functions/src/routes/user.ts
+++ functions/src/routes/user.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { getUsers, getCurrentUser } from "../controllers/user_controller";
+import { getUsers, getCurrentUser, deleteAccount } from "../controllers/user_controller";
 import {convertRequestToCamelCase} from "../utils/camel_case";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.use(convertRequestToCamelCase);
 
 router.get("/", (req: Request, res: Response) => getUsers(req, res));
 router.get("/me", (req: Request, res: Response) => getCurrentUser(req, res));
+router.delete("/me", (req: Request, res: Response) => deleteAccount(req, res));
 
 export default router;
