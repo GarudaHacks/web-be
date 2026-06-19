325fc0bef12065b787e3cae809018f20c66f719d
RyanDJ <heryandjaruma@gmail.com>
2026-06-19 13:13:57 +0800

    fix lint

--- functions/src/templates/applicationSubmitted.ts
+++ functions/src/templates/applicationSubmitted.ts
@@ -3,6 +3,10 @@ import { join } from "path";
 
 const template = readFileSync(join(__dirname, "applicationSubmitted.html"), "utf-8");
 
+/**
+ * Return a template for Application Submitted.
+ * @returns 
+ */
 export function applicationSubmitted(): string {
   return template
 }
