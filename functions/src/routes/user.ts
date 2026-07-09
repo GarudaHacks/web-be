d594cc6fa0e68fac3adee2f59fd5f0cbbe23e8e0
RyanDJ <heryandjaruma@gmail.com>
2026-07-09 21:38:53 +0800

    remove sensitive endpoint

--- functions/src/routes/user.ts
+++ functions/src/routes/user.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { getUsers, getCurrentUser, deleteAccount, getBoardingPassInfo } from "../controllers/user_controller";
+import { getCurrentUser, deleteAccount, getBoardingPassInfo } from "../controllers/user_controller";
 import {convertRequestToCamelCase} from "../utils/camel_case";
 import { isConfirmedRSVP } from "../middlewares/role_middleware";
 
@@ -7,7 +7,6 @@ const router = express.Router();
 
 router.use(convertRequestToCamelCase);
 
-router.get("/", (req: Request, res: Response) => getUsers(req, res));
 router.get("/me", (req: Request, res: Response) => getCurrentUser(req, res));
 router.delete("/me", (req: Request, res: Response) => deleteAccount(req, res));
 router.get("/boarding-pass", isConfirmedRSVP, (req: Request, res: Response) => getBoardingPassInfo(req, res))
